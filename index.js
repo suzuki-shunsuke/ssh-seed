f7e5df5e6b7433e872676babfed3c94f302dae84
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-03-02 11:08:31 +0900

    feat: Find and read configuraiton file and execute core scripts

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const co = require('co');
 const util = require('./lib/util.js');
 
+const CONFIG_FILE_NAME = 'ssh-seed.yml';
+
 const main = function* (argv) {
   if (argv.help) {
     return process.stdout.write(yield util.help());
   }
   if (argv.version) {
     return process.stdout.write(yield util.version());
+  }
+  const config = (yield util.findConf(process.cwd(), CONFIG_FILE_NAME)) || {
+    keys: [],
+  };
+  for (const key of config.keys) {
+    util.execScript(key, () => {});
   }
 };
 
