b2b4ca0d11e458bf9ea991b1d817eb820abbedf9
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-03-06 00:24:18 +0900

    feat: Add the "--reqexp" option
    It is enable to filter target authentication keys
    by the regular expression.

--- index.js
+++ index.js
@@ -34,7 +34,16 @@ const main = function* (argv) {
     // if pass file is not found, do nothing
   }
 
+  let regexp = false;
+  try {
+    regexp = argv.regexp ? new RegExp(argv.regexp) : false;
+  } catch (e) {
+    util.writeErrLn(`ssh-seed:Error: the value of the regexp option "${argv.regexp}" is invalid for the regular expression.`);
+    process.exit(1);
+  }
+
   for (const key in config.config.keys) {
+    if (regexp && !regexp.test(key)) { continue; }
     const filePath = path.isAbsolute(key) ? key : path.join(config.path, key);
     yield cmd.func(key, filePath, config, passFilePath, passphrases);
   }
