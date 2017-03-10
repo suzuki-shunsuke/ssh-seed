657bb8e323bdcf259dde9e6d49ad834471b7f270
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-03-10 09:23:32 +0900

    fix: Fix setup-release to set the author name and email to commits

--- setup-release.sh
+++ setup-release.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -eu
 
+COMMIT_USER=`git config user.name`
+COMMIT_EMAIL=`git config user.email`
 if [ ! -d release ]; then
     git clone https://github.com/suzuki-shunsuke/ssh-seed --single-branch --branch master release
 fi
 cd release
+git config user.name "$COMMIT_USER"
+git config user.email "$COMMIT_EMAIL"
 git pull origin master
 npm run setup
 npm run standard-version
